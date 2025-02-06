3b3ef8a25292737edb6916fea5574e73184cde1f
Tom Cannaerts <tom.cannaerts@iodigital.com>
2025-02-06 19:57:23 +0100

    Codestyles + error handling / stan

--- build/build.php
+++ build/build.php
@@ -7,81 +7,94 @@ $icann_list = [];
 $private_list = [];
 
 $icann = true;
-$list = file_get_contents($url);
-foreach (explode("\n", $list) as $line) {
-    if ('' != trim($line) && '//' != substr($line, 0, 2)) {
-        if ($icann) {
-            $icann_list[] = trim($line);
-        } else {
-            $private_list[] = trim($line);
+if ($list = file_get_contents($url)) {
+    foreach (explode("\n", $list) as $line) {
+        if ('' != trim($line) && '//' != substr($line, 0, 2)) {
+            if ($icann) {
+                $icann_list[] = trim($line);
+            } else {
+                $private_list[] = trim($line);
+            }
+        } elseif (trim($line) == $icann_end) {
+            // end of ICANN list
+            $icann = false;
         }
-    } elseif (trim($line) == $icann_end) {
-        // end of ICANN list
-        $icann = false;
     }
-}
 
-$meta = [];
-$data = explode("\n", file_get_contents(__DIR__.'/meta.txt'));
-foreach ($data as $line) {
-    $var = explode('=', $line);
-    if (count($var) > 1) {
-        $meta[$var[0]] = $var[1];
+    $meta = [];
+    if ($metaContent = file_get_contents(__DIR__.'/meta.txt')) {
+        $data = explode("\n", $metaContent);
+        foreach ($data as $line) {
+            $var = explode('=', $line);
+            if (count($var) > 1) {
+                $meta[$var[0]] = $var[1];
+            }
+        }
+    } else {
+        $meta = [
+            'version' => '1.0.0',
+            'icann' => '',
+            'full' => '',
+        ];
     }
-}
-$updated = false;
+    $updated = false;
 
-// write classes
-$name = 'PSLIcann';
-$values = "'icann' => ['".implode("', '", $icann_list)."']";
-$content = <<<EOF
-<?php
-
-namespace TomCan\PublicSuffixList;
-
-class $name extends AbstractPSL
-{
-    protected \$lists = [$values];
-}
-
-EOF;
-
-if (sha1($content) != $meta['icann']) {
-    $updated = true;
-    $meta['icann'] = sha1($content);
-    file_put_contents(__DIR__.'/../src/'.$name.'.php', $content);
-}
-
-$name = 'PSLFull';
-$values = "'icann' => ['".implode("', '", $icann_list)."'], 'private' => ['".implode("', '", $private_list)."']";
-$content = <<<EOF
-<?php
-
-namespace TomCan\PublicSuffixList;
-
-class $name extends AbstractPSL
-{
-    protected \$lists = [$values];
-}
-
-EOF;
-
-if (sha1($content) != $meta['full']) {
-    $updated = true;
-    $meta['full'] = sha1($content);
-    file_put_contents(__DIR__.'/../src/'.$name.'.php', $content);
-}
-
-if ($updated) {
-    // version bump
-    $version = explode('.', $meta['version']);
-    ++$version[count($version) - 1];
-    $meta['version'] = implode('.', $version);
-
-    // rewrite meta.txt
-    $content = '';
-    foreach ($meta as $key => $value) {
-        $content .= "$key=$value\n";
+    // write classes
+    $name = 'PSLIcann';
+    $values = "'icann' => ['".implode("', '", $icann_list)."']";
+    $content = <<<EOF
+    <?php
+    
+    namespace TomCan\PublicSuffixList;
+    
+    class $name extends AbstractPSL
+    {
+        protected \$lists = [$values];
     }
-    file_put_contents(__DIR__.'/meta.txt', $content);
+    
+    EOF;
+
+    if (sha1($content) != $meta['icann']) {
+        $updated = true;
+        $meta['icann'] = sha1($content);
+        file_put_contents(__DIR__.'/../src/'.$name.'.php', $content);
+    }
+
+    $name = 'PSLFull';
+    $values = "'icann' => ['".implode("', '", $icann_list)."'], 'private' => ['".implode("', '", $private_list)."']";
+    $content = <<<EOF
+    <?php
+    
+    namespace TomCan\PublicSuffixList;
+    
+    class $name extends AbstractPSL
+    {
+        protected \$lists = [$values];
+    }
+    
+    EOF;
+
+    if (sha1($content) != $meta['full']) {
+        $updated = true;
+        $meta['full'] = sha1($content);
+        file_put_contents(__DIR__.'/../src/'.$name.'.php', $content);
+    }
+
+    if ($updated) {
+        // version bump
+        $version = explode('.', $meta['version']);
+        ++$version[count($version) - 1];
+        $meta['version'] = implode('.', $version);
+
+        // rewrite meta.txt
+        $content = '';
+        foreach ($meta as $key => $value) {
+            $content .= "$key=$value\n";
+        }
+        if (false !== file_put_contents(__DIR__.'/meta.txt', $content)) {
+            throw new RuntimeException('Could not save meta data to meta.txt.');
+        }
+    }
+} else {
+    throw new RuntimeException('Could not download public prefix list.');
 }
